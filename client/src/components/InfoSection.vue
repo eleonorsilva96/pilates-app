fedb345711262c0761c457513a0f46f030054f95
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-07-24 17:47:03 +0100

    add components in HomeView

---
+++ client/src/components/InfoSection.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HomeCard from './HomeCard.vue';
+import ButtonsCTA from './ButtonsCTA.vue';
+
+const props = defineProps(['bg-pink', 'title', 'description', 'is-card'])
+// const props = defineProps({
+//     title: String,
+//     description: String,
+//     isCard: Boolean
+// })
+
+</script>
+
+<template>
+    <div :class="['container', 'mx-auto', 'px-4', 'py-20', props.bgPink]">
+        <div class="flex flex-col gap-8 align items-center">
+            <h1 class="text-5xl md:text-6xl font-bold max-w-2xl text-center">{{ props.title }}</h1>
+            <p class="text-xl text-gray-500 text-lg max-w-2xl text-center">{{ props.description }}</p>
+            <!-- HomeCard or ButtonsCTA -->
+             <HomeCard v-if="props.isCard"/>
+             <ButtonsCTA v-else/>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import InfoSection from '../components/InfoSection.vue'
+import HomeCard from '../components/HomeCard.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="min-h-screen bg-white">
+    <InfoSection title="Transform Your Body with
+                PilatesFlow" description="Join our community of pilates enthusiasts and
+                discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
+                :is-card="false"/>
+    <InfoSection bg-pink="bg-pink-100" title="Transform Your Body with
+                PilatesFlow" description="Join our community of pilates enthusiasts and
+                discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
+                :is-card="true"/>
+    <InfoSection title="Transform Your Body with
+                PilatesFlow" description="Join our community of pilates enthusiasts and
+                discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
+                :is-card="false"/>
+  </div>
 </template>
