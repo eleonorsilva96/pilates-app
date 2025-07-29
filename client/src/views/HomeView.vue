6d30738c10fbc2e5a6b13ace6481d5cccfd5fcd8
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-07-29 19:09:09 +0100

    toogle buttons and icons display

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,17 +5,17 @@ import HomeCard from '../components/HomeCard.vue'
 
 <template>
   <div class="min-h-screen bg-white">
-    <InfoSection title="Transform Your Body with
-                PilatesFlow" description="Join our community of pilates enthusiasts and
-                discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
-                :is-card="false"/>
     <InfoSection bg-pink="bg-pink-100" title="Transform Your Body with
                 PilatesFlow" description="Join our community of pilates enthusiasts and
                 discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
-                :is-card="true"/>
+                :is-card="false" :is-single-btn="false"/>
     <InfoSection title="Transform Your Body with
                 PilatesFlow" description="Join our community of pilates enthusiasts and
                 discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
-                :is-card="false"/>
+                :is-card="true" :is-single-btn="false"/>
+    <InfoSection bg-pink="bg-pink-100" title="Transform Your Body with
+                PilatesFlow" description="Join our community of pilates enthusiasts and
+                discover the perfect balance of strength, flexibility, and mindfulness with our expert instructors." 
+                :is-card="false" :is-single-btn="true"/>
   </div>
 </template>
