df49906af3143726abcc6d5aefcb63c07af77a6f
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-08-01 16:19:16 +0100

    use RouterLink as the hyperlink element in Header

--- client/src/App.vue
+++ client/src/App.vue
@@ -20,17 +20,13 @@ onMounted(async () => {
     <div class="border-b sticky">
       <div class="container mx-auto px-4 sm:px-6 lg:px-8">
         <nav class="flex justify-between h-16">
-          <a href="" class="self-center"><span class="font-bold text-xl"><RouterLink to="/">PilatesFlow</RouterLink></span></a>
+          <RouterLink to="/" class="self-center font-bold text-xl">PilatesFlow</RouterLink>
           <div class="flex space-x-4 items-center">
-            <a href="" class="font-medium text-pink-600 dark:text-pink-500 hover:underline"
-              ><RouterLink to="/auth/login">Login</RouterLink></a
-            >
-            <a
-              href=""
-              class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-pink-600 dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-800"
-            >
-              <RouterLink to="/auth/register">Get Started</RouterLink></a
-            >
+            <RouterLink to="/auth/login" class="font-medium text-pink-600 dark:text-pink-500 hover:underline">Login
+            </RouterLink>
+            <RouterLink to="/auth/register"
+              class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-pink-600 dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-800">
+              Get Started</RouterLink>
           </div>
         </nav>
       </div>
