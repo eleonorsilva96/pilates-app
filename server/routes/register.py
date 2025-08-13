c977a0166f409754ecb0283c7c4f9a9a07ca2a4b
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-08-13 23:49:16 +0100

    add user validation in register

--- server/routes/register.py
+++ server/routes/register.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
 from pydantic import BaseModel, field_validator, constr, ValidationError
-from models import User
-from extensions import db, bcrypt
+from models import Users
+from extensions import db, bcrypt, EmailAlreadyExistsError
+from sqlalchemy import select
 import re # regular expressions module
 
 # Creates a blueprint that can be imported
@@ -10,7 +11,7 @@ register_bp = Blueprint('register_bp', __name__)
 NAME_PATTERN = r"^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$"
 EMAIL_PATTERN = r"^[\w\.-]+@[\w\.-]+\.\w+$"
 
-class UserSchema(BaseModel):
+class UsersSchema(BaseModel):
     fullname: constr(strip_whitespace=True)
     email: constr(strip_whitespace=True)
     password: constr(min_length=5)
@@ -31,16 +32,20 @@ class UserSchema(BaseModel):
 @register_bp.route('/register', methods=["POST"])
 def register():
     try:
-        user = UserSchema(**request.json) # raise ValidationError if not passed
+        data = UsersSchema(**request.json) # raise ValidationError if not passed
 
+        # check for existed user
+        user = db.session.scalars(select(Users).where(Users.email == data.email)).first()
+        
+        if user:
+            raise EmailAlreadyExistsError("This email already exists")
+        
         # hash the password
-        hashed_pw = bcrypt.generate_password_hash(user.password).decode("utf-8")
+        hashed_pw = bcrypt.generate_password_hash(data.password).decode("utf-8")
 
-        print(hashed_pw)
-
-        new_user = User(
-            name=user.fullname,
-            email=user.email,
+        new_user = Users(
+            name=data.fullname,
+            email=data.email,
             password_hash=hashed_pw
         )
 
@@ -56,3 +61,6 @@ def register():
         error_messages = list(errors.values())
         
         return jsonify({"errors": error_messages}), 400
+    except EmailAlreadyExistsError as e:
+        # return custom error inside the same errors list
+        return jsonify({"errors": [str(e)]}), 400
