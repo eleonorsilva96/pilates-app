4905b3d4d0e7dfc03e0f653a68ba047c9ae6fcd4
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-08-13 23:43:59 +0100

    insert alert messages into functions

--- client/src/helpers/generalHelpers.js
+++ client/src/helpers/generalHelpers.js
@@ -7,3 +7,22 @@ export function showAlert(visible, type, message) {
     message.value = []
   }, 7000)
 }
+
+
+export function showSuccessAlert(msg, isVisible, type, message) {
+
+    type.value.push('success')
+    message.value.push(msg)
+    showAlert(isVisible, type, message)
+
+}
+
+export function showErrorAlert(err, isVisible, type, message) {
+
+  err.response.data.errors.forEach((msg) => {
+        type.value.push('error')
+        message.value.push(msg)
+        showAlert(isVisible, type, message)
+      })
+
+}
