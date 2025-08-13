98b9572df5fcf79bc404af19655b8d67d5205054
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-08-13 23:46:34 +0100

    add Dashboard view and route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AuthView from '../views/AuthView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
 
         { path: 'register', name: 'register', component: RegisterView },
       ],
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
     },
   ],
 })
