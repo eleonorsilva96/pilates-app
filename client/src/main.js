0fcb50dd6c92389178a68076d7f6518c4254590c
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-10-14 12:32:57 +0100

    install vue-datepicker

--- client/src/main.js
+++ client/src/main.js
@@ -2,15 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
+
+app.component('VueDatePicker', VueDatePicker)
 
 app.use(pinia)
 app.use(router)
