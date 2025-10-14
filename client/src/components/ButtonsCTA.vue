8b5142be360683472ba7a40a45d5c80d34282e60
eleonorsilva96 <eleonor.uxui@gmail.com>
2025-10-14 13:18:11 +0100

    adjust ButtonCTA component

--- client/src/components/ButtonsCTA.vue
+++ client/src/components/ButtonsCTA.vue
@@ -1,21 +1,29 @@
 <!-- adjust it, use only the button -->
 
 <script setup>
-const props = defineProps(['is-single-btn'])
+// TODO: Add props to style the button
+const { isDisable, buttonType } = defineProps({
+  isDisable: Boolean,
+  buttonType: String
+})
 </script>
 <template>
   <div class="flex flex-col sm:flex-row gap-4 justify-center">
     <button
-      class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-base px-5 py-3 dark:bg-pink-600 dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-800"
-    >
-      Start your Jouney
+    :type="buttonType"
+      class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 
+      font-medium rounded-lg text-base px-5 py-3 dark:bg-pink-600 dark:hover:bg-pink-700 
+      focus:outline-none dark:focus:ring-pink-800 w-full disabled:opacity-50 disabled:cursor-not-allowed"
+    :disabled="isDisable"
+      >
+      <slot></slot>
     </button>
-    <button
-      v-if="!props.isSingleBtn"
+    <!-- <button
+      v-if="!isSingleBtn"
       type="button"
       class="text-pink-700 hover:text-white bg-white border border-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-base px-5 py-3 text-center dark:border-pink-500 dark:text-pink-500 dark:hover:text-white dark:hover:bg-pink-500 dark:focus:ring-pink-800"
     >
       Meet Our Classes
-    </button>
+    </button> -->
   </div>
 </template>
